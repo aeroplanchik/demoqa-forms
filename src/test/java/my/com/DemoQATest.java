6c57123ba67de0ce4c3add2ca97059359310cc66
AA <aa@aa.com>
2021-09-02 13:16:48 +0300

    first example commit. add README.md test

---
+++ src/test/java/my/com/DemoQATest.java
@@ -0,0 +1,27 @@
+package my.com;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DemoQATest {
+
+   @BeforeEach
+
+       void beforeEach () {
+           System.out.println("Before test! \n");
+       }
+   @AfterEach
+    void afterEach () {
+       System.out.println("After test! \n");
+   }
+
+    @Test
+    void  firstTest() {
+
+       System.out.println("I love you QA.GURU \n");
+
+   }
+
+}
