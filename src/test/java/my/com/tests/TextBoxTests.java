85c704414d9d7b872d94afa93d2212511bebea64
AA <aa@aa.com>
2021-09-02 15:13:24 +0300

    add TextBoxTests

--- src/test/java/my/com/tests/TextBoxTests.java
+++ src/test/java/my/com/tests/TextBoxTests.java
@@ -1,16 +1,34 @@
 package my.com.tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
 
     @Test
     void fillFormTest() {
+        String permanentAddress = "some street 1";
 
         open("https://demoqa.com/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@qaguru.com");
+        $("#currentAddress").setValue("some street 1");
+        $("#permanentAddress").setValue("another street 2");
+        $("#submit").click();
 
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@qaguru.com"));
+        $("#output #currentAddress").shouldHave(text(permanentAddress));
+        $("#output #permanentAddress").shouldHave(text("another street 2"));
 
     }
 }
