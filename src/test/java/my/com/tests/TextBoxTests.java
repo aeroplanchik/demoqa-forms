862af8eb82573938c11bdfc78d563f11dd973d9f
AA <aa@aa.com>
2021-09-02 14:25:29 +0300

    renamed junit examples

---
+++ src/test/java/my/com/tests/TextBoxTests.java
@@ -0,0 +1,17 @@
+package my.com.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @Test
+    void fillFormTest() {
+
+        open("https://demoqa.com/text-box");
+
+
+
+    }
+}
